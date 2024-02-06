5f9ce79b01d658cf9be25768f52df30aefbd0f49
subh05sus <sahasubhadipfkk@gmail.com>
2024-02-06 22:12:42 +0530

    added layout andd header

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+
+
+const Layout = () => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header/>
+        </div>
+    )
+}
+
+export default Layout
