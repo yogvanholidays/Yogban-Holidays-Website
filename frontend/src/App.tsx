b6e8fcfacc254792a4df07fa19e28fb2ea909d8f
subh05sus <sahasubhadipfkk@gmail.com>
2024-02-12 23:37:06 +0530

    seach implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 function App() {
   const {isLoggedIn} = useAppContext()
@@ -18,7 +19,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout>Home Page</Layout>} />
-        <Route path="/search" element={<Layout>Search Page</Layout>} />
+        <Route path="/search" element={<Layout><Search/></Layout>} />
         <Route path="/register" element={<Layout><Register/></Layout>} />
         <Route path="/sign-in" element={<Layout><SignIn/></Layout>} />
         {isLoggedIn && <>
