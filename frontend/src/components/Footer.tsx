ff650ce67ce8281d2e7bd0a31d5256c0eae948f0
subh05sus <sahasubhadipfkk@gmail.com>
2024-02-20 18:53:51 +0530

    updated responsive footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,16 +2,19 @@ import { Link } from "react-router-dom"
 
 const Footer = () =>{
     return (
-        <div className="bg-red-900 py-10">
-            <div className="container mx-auto flex justify-between items-center">
+        <div className="bg-red-900 py-10 flex flex-col items-center">
+            <div className="container mx-auto flex justify-between items-center flex-col 2xl:flex-row">
                 <span className="text-3xl text-white font-bold tracking-tight">
                     <Link to='/'>YogbanHolidays.com</Link>
                 </span>
-                <span className="text-white font-bold tracking-tight flex gap-4">
+                <span className="text-white font-bold tracking-tight flex gap-4 mt-2 2xl:mt-0">
                     <p className="cursor-pointer">Privacy Policy</p>
                     <p className="cursor-pointer">Terms of Service</p>
                 </span>
             </div>
+            <span className="text-white text-xs tracking-tight flex gap-4 mt-4 -mb-8">
+                    <p className="cursor-pointer">developed by Batloop</p>
+                </span>
         </div>
     )
 }
