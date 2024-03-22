dccc3e8de11dc180765284896307cc54b1018cd3
subh05sus <sahasubhadipfkk@gmail.com>
2024-03-23 00:55:59 +0530

    lol

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -10,7 +10,7 @@ import {
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://yogban.vercel.app";
 
 export const fetchCurrentUser = async (): Promise<UserType> => {
   const response = await fetch(`${API_BASE_URL}/api/users/me`, {
