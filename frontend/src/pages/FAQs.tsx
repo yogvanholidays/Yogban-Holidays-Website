1a78e61a495590cfc8843386462cfeea090a6296
subh05sus <sahasubhadipfkk@gmail.com>
2025-01-13 10:18:51 +0530

    updated FAQs with new questions and answers for Yogvan Holidays 1BHK Apartments Tapovan Rishikesh

--- frontend/src/pages/FAQs.tsx
+++ frontend/src/pages/FAQs.tsx
@@ -7,23 +7,53 @@ import FAQ from "../components/FAQ";
 function Faqs() {
   const [faqs, setFaqs] = useState([
     {
-      question: "How many programmers does it take to screw a lightbulb?",
-      answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-      open: true
+      question: "Is Yogvan Holidays 1BHK Apartments Tapovan Rishikesh popular with families?",
+      answer: "Yes, Yogvan Holidays 1BHK Apartments Tapovan Rishikesh is popular with guests booking family stays.",
+      open: true,
     },
     {
-      question: "Who is the most awesome person?",
-      answer: "You! The viewer!",
-      open: false
+      question: "How much does it cost to stay at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh?",
+      answer: "The prices at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh may vary depending on your stay (e.g. dates, hotel's policy etc.). To see prices, enter your dates.",
+      open: false,
     },
     {
-      question:
-        "How many questions does it take to makes a succesful FAQ Page?",
-      answer: "This many!",
-      open: false
-    }
+      question: "How many bedrooms does Yogvan Holidays 1BHK Apartments Tapovan Rishikesh have?",
+      answer: "Yogvan Holidays 1BHK Apartments Tapovan Rishikesh has the following number of bedrooms: 1 bedroom. For more detailed info, check the accommodation option(s) breakdown on this page.",
+      open: false,
+    },
+    {
+      question: "Does Yogvan Holidays 1BHK Apartments Tapovan Rishikesh have a terrace?",
+      answer: "Yes, there are options at this property that have a terrace. You can find out more about this and the other facilities at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh on this page.",
+      open: false,
+    },
+    {
+      question: "Does Yogvan Holidays 1BHK Apartments Tapovan Rishikesh have a balcony?",
+      answer: "Yes, there are options at this property that have a balcony. You can find out more about this and the other facilities at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh on this page.",
+      open: false,
+    },
+    {
+      question: "How many guests can sleep at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh?",
+      answer: "You can choose from a number of accommodation options at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh (subject to availability) that can accommodate: 2 guests, 4 guests. For more detailed info, check the accommodation option(s) breakdown on this page.",
+      open: false,
+    },
+    {
+      question: "How far is Yogvan Holidays 1BHK Apartments Tapovan Rishikesh from the center of Rishīkesh?",
+      answer: "The exact distance from Yogvan Holidays 1BHK Apartments Tapovan Rishikesh to the center of Rishīkesh is not mentioned, but you can find detailed location information on this page.",
+      open: false,
+    },
+    {
+      question: "What is there to do at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh?",
+      answer: "Yogvan Holidays 1BHK Apartments Tapovan Rishikesh offers the following activities/services (charges may apply): Hiking, Bicycle rental, Water sports facilities on site.",
+      open: false,
+    },
+    {
+      question: "What are the check-in and check-out times at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh?",
+      answer: "Check-in at Yogvan Holidays 1BHK Apartments Tapovan Rishikesh is from 2:00 PM, and check-out is until 10:30 AM.",
+      open: false,
+    },
+
   ]);
+  
 
   const toggleFAQ = (index:number) => {
     setFaqs(
